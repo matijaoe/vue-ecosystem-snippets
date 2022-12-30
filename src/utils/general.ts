63c1f0907f4704dd78a2161379b60bf2a31456a0
Matija Osrečki <matija.osrecki@gmail.com>
2022-12-30 23:21:58 +0100

    Fix tab indentation in docs snippet examples (#5)
    * Fix docs snippet column tab indentation
    
    * Bump to v0.3.1

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -6,3 +6,5 @@ export const addSymbol = (str: string) => `${str} ${SYMBOL}`;
 export const parseMultiline = (s: string | string[]) => {
   return Array.isArray(s) ? s.join("\n") : s;
 };
+
+export const replaceTabs = (s: string) => s.replace(/\t/g, "  ");
