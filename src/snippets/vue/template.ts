eee2be1d85379e6d611ca0c148ce6f13b97581dd
Matija Osrečki <matija.osrecki@gmail.com>
2023-01-03 18:56:45 +0100

    Add vue class & style bindings

--- src/snippets/vue/template.ts
+++ src/snippets/vue/template.ts
@@ -4,7 +4,6 @@ import { XSnippetDefinition } from "../../models/app.ts";
 /**
  * - transition classes and events
  * - router link
- * - class bindings
  */
 export const template: XSnippetDefinition = {
   meta: {
@@ -19,6 +18,10 @@ export const template: XSnippetDefinition = {
     template: {
       name: "template",
       body: "<template>$0</template>",
+    },
+    component: {
+      name: "component",
+      body: "<component>$0</component>",
     },
     nslot: {
       name: "named slot",
@@ -50,6 +53,44 @@ export const template: XSnippetDefinition = {
       name: "Vue teleport",
       body: '<Teleport to="$1">\n\t$0\n</Teleport>',
     },
+    vTransition: {
+      name: "Vue Transition",
+      body: "<Transition $1>\n\t$0\n</Transition>",
+    },
+    "vTransition:name": {
+      name: "Vue Transition with name",
+      body: '<Transition name="$1" $2>\n\t$0\n</Transition>',
+      alt: ["nTransition"],
+    },
+    "vTransition:type": {
+      name: "Vue Transition with type",
+      body:
+        '<Transition type="${1|transition,animation|}" $2>\n\t$0\n</Transition>',
+    },
+    "vTransition:appear": {
+      name: "Vue Transition with appear",
+      body: "<Transition appear $1>\n\t$0\n</Transition>",
+    },
+    vTransitionGroup: {
+      name: "Vue TransitionGroup",
+      body:
+        '<TransitionGroup name="$1" as="${2|ul,div,section|}" $3>\n\t$0\n</TransitionGroup>',
+    },
+    vSuspense: {
+      name: "Vue Suspense",
+      body: "<Suspense>\n\t$0\n</Suspense>",
+    },
+    "vSuspense:fallback": {
+      name: "Vue Suspense with fallback",
+      body: [
+        "<Suspense>",
+        "\t$0",
+        "\t<template #fallback>",
+        "\t\t$1",
+        "\t</template>",
+        "</Suspense>",
+      ],
+    },
     vtext: {
       name: "v-text",
       body: 'v-text="$1"',
@@ -65,14 +106,17 @@ export const template: XSnippetDefinition = {
     vif: {
       name: "v-if",
       body: 'v-if="$1"',
+      alt: ["if"],
     },
     velse: {
       name: "v-else",
       body: "v-else",
+      alt: ["else"],
     },
     velif: {
       name: "v-else-if",
       body: 'v-else-if="$1"',
+      alt: ["elif"],
     },
     vfor: {
       name: "v-for",
@@ -138,13 +182,9 @@ export const template: XSnippetDefinition = {
       name: "Vue ref",
       body: 'ref="$1"',
     },
-    vto: {
-      name: "router link :to prop",
-      body: '${1|to,:to|}="$2"',
-    },
     vname: {
       name: "name property",
-      body: 'name="$1"',
+      body: '${1|name,:name|}="$1"',
     },
     vis: {
       name: "is property",
@@ -154,8 +194,30 @@ export const template: XSnippetDefinition = {
       name: "bind property",
       body: ':${1:prop}="$2"',
     },
-    "von-emit": {
-      name: "emit event",
+    vclass: {
+      name: "Vue classes",
+      body: `:class="[$1]"`,
+      alt: ["vc"],
+    },
+    "vclass:cond": {
+      name: "Vue conditional classes",
+      body: `:class="{ $1: $2 }"`,
+      alt: ["vcc"],
+    },
+    vstyle: {
+      name: "Vue inline style",
+      body: `:style="{ $1: $2 }"`,
+    },
+    "vstyle:list": {
+      name: "Vue inline style list",
+      body: `:style="[$1]"`,
+    },
+    vti: {
+      name: "Vue text interpolation",
+      body: "{{ $1 }}",
+    },
+    vemit: {
+      name: "Vue emit event",
       body:
         "@${1|click,input,change,focus,blur|}=\"\\$emit('${2:$1}', \\$event)\"",
     },
@@ -166,10 +228,6 @@ export const template: XSnippetDefinition = {
     "vif:slot-prop": {
       name: "v-if slot or prop defined",
       body: 'v-if="\\$slots.${1:label} || ${2:$1}"',
-    },
-    vti: {
-      name: "Vue text interpolation",
-      body: "{{ $1 }}",
     },
     vform: {
       name: "form submit.prevent",
@@ -202,33 +260,6 @@ export const template: XSnippetDefinition = {
         "\t$0",
         "</$1>",
       ],
-    },
-    vTransition: {
-      name: "Vue Transition",
-      body: "<Transition $1>\n\t$0\n</Transition>",
-    },
-    "vTransition:name": {
-      name: "Vue Transition with name",
-      body: '<Transition name="$1" $2>\n\t$0\n</Transition>',
-      alt: ["nTransition"],
-    },
-    "vTransition:type": {
-      name: "Vue Transition with type",
-      body:
-        '<Transition type="${1|transition,animation|}" $2>\n\t$0\n</Transition>',
-    },
-    "vTransition:appear": {
-      name: "Vue Transition with appear",
-      body: "<Transition appear $1>\n\t$0\n</Transition>",
-    },
-    vTransitionGroup: {
-      name: "Vue TransitionGroup",
-      body:
-        '<TransitionGroup name="$1" as="${2|ul,div,section|}" $3>\n\t$0\n</TransitionGroup>',
-    },
-    vSuspense: {
-      name: "Vue Suspense",
-      body: "<Suspense>\n\t$0\n</Suspense>",
     },
   },
 };
