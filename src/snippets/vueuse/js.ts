36546c4dd4901a5533fb909eadde813309884991
Matija Osrečki <matija.osrecki@gmail.com>
2023-01-04 20:45:06 +0100

    Cleanup VueUse, add `vuf` alt for `vuse:fetch`

--- src/snippets/vueuse/js.ts
+++ src/snippets/vueuse/js.ts
@@ -7,7 +7,7 @@ import { XSnippetDefinition } from "../../models/app.ts";
  * - array methods
  * - date format
  * - timeago
- * - get, set
+ * - VueUse components
  */
 export const vueUseJs: XSnippetDefinition = {
   meta: {
@@ -15,56 +15,56 @@ export const vueUseJs: XSnippetDefinition = {
   },
   snippets: {
     "vwatchArray": {
-      "name": "VueUse watchArray",
-      "body":
+      name: "VueUse watchArray",
+      body:
         "watchArray(${1:list}, (new${1/(.*)/${1:/capitalize}/}, old${1/(.*)/${1:/capitalize}/}, ${4:added}, ${5:removed}) => {\n\t$0\n})",
     },
     "vwatchAtMost": {
-      "name": "VueUse watchAtMost",
-      "body":
+      name: "VueUse watchAtMost",
+      body:
         "watchAtMost(${1:source}, (${2:val}) => {\n\t$0\n}, { max: ${3:3} })",
     },
     "vwatchDebounced": {
-      "name": "VueUse watchDebounced",
-      "body":
+      name: "VueUse watchDebounced",
+      body:
         "watchDebounced(${1:source}, (${2:val}) => {\n\t$0\n}, { debounce: ${3:500}, maxWait: ${4:1000} })",
     },
     "vwatchIgnorable": {
-      "name": "VueUse watchIgnorable",
-      "body":
+      name: "VueUse watchIgnorable",
+      body:
         "const { stop:$3, ignoreUpdates:$4 } =  = watchIgnorable(${1:source}, (${2:val}) => {\n\t$0\n})",
     },
     "vwatchOnce": {
-      "name": "VueUse watchOnce",
-      "body": "watchOnce(${1:source}, (${2:val}) => {\n\t$0\n})",
+      name: "VueUse watchOnce",
+      body: "watchOnce(${1:source}, (${2:val}) => {\n\t$0\n})",
     },
     "vwatchPausable": {
-      "name": "VueUse watchPausable",
-      "body":
+      name: "VueUse watchPausable",
+      body:
         "const { stop$3, pause$4, resume$5 } = watchPausable(${1:source}, (${2:val}) => {\n\t$0\n})",
     },
     "vwatchThrottled": {
-      "name": "VueUse watchThrottled",
-      "body":
+      name: "VueUse watchThrottled",
+      body:
         "watchThrottled(${1:source}, (${2:val}) => {\n\t$0\n}, { throttle: ${3:500} })",
     },
     "vwatchTriggerable": {
-      "name": "VueUse watchTriggerable",
-      "body":
+      name: "VueUse watchTriggerable",
+      body:
         "const { trigger$3, ignoreUpdates$4 } = watchTriggerable(${1:source}, (${2:val}) => {\n\t$0\n})",
     },
     "vwatchWithFilter": {
-      "name": "VueUse watchWithFilter",
-      "body":
+      name: "VueUse watchWithFilter",
+      body:
         "watchWithFilter(${1:source}, (${2:val}) => {\n\t$0\n}, { eventFilter: $3 })",
     },
     "vwhenever": {
-      "name": "VueUse whenever",
-      "body": "whenever(${1:source}, (${2:val}) => {\n\t$0\n})",
+      name: "VueUse whenever",
+      body: "whenever(${1:source}, (${2:val}) => {\n\t$0\n})",
     },
     "vuse:vmodel": {
-      "name": "Implement v-model using useVModel",
-      "body": [
+      name: "Implement v-model using useVModel",
+      body: [
         "const props = defineProps<{",
         "\t${1:modelValue}: ${2:string}",
         "}>()",
@@ -77,23 +77,24 @@ export const vueUseJs: XSnippetDefinition = {
       ],
     },
     "vuse:hover": {
-      "name": "VueUse useElementHover",
-      "body": [
+      name: "VueUse useElementHover",
+      body: [
         "const ${1:elem} = ref<${2|HTMLInputElement,HTMLDivElement,HTMLImageElement,HTMLFormElement|} | null>(null)",
         "const ${3:isHovered} = useElementHover($1)",
       ],
     },
     "vuse:fetch": {
-      "name": "VueUse useFetch",
-      "body": "const { data: ${1:data} } = useFetch('$3')",
+      name: "VueUse useFetch",
+      body: "const { data: ${1:data} } = useFetch('$2'$3)",
+      alt: ["vuf"],
     },
-    "vget": {
-      "name": "VueUse get",
-      "body": "get($1)",
+    vget: {
+      name: "VueUse get",
+      body: "get($1)",
     },
-    "vset": {
-      "name": "VueUse set",
-      "body": "set($1, $2)",
+    vset: {
+      name: "VueUse set",
+      body: "set($1, $2)",
     },
   },
 };
